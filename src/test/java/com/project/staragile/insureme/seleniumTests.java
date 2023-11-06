07a26a8430447b9cd70b23a5b1839bddd69c14a1
asambajava <96311074+asambajava@users.noreply.github.com>
2023-11-05 22:27:34 -0500

    seleniumtests
    tests

---
+++ src/test/java/com/project/staragile/insureme/seleniumTests.java
@@ -0,0 +1,34 @@
+package com.project.staragile.insureme;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class seleniumTests {
+	@Test
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver-mac-arm64/chromedriver");
+	ChromeOptions chromeoptions = new ChromeOptions();
+	WebDriver driver=new ChromeDriver(chromeoptions);
+	driver.get("http://54.87.202.235:8081/contact.html");
+	WebElement name=driver.findElement(By.id("inputName"));
+	WebElement number=driver.findElement(By.id("inputNumber"));
+	WebElement mail=driver.findElement(By.name("inputMail"));
+	WebElement message=driver.findElement(By.name("inputMessage"));
+	WebElement send=driver.findElement(By.name("my-button"));
+	WebElement resp=driver.findElement(By.name("response"));
+	name.sendKeys("abc");
+	number.sendKeys("12345");
+	mail.sendKeys("abc12345@gmail.com");
+	message.sendKeys("hello");
+	send.click();
+	String actualMessage="Message Sent";
+	String expectedMessage= resp.getText();
+	Assert.assertEquals(actualMessage,expectedMessage);
+	driver.close();
+	}
+
+}
